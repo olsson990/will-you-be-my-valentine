250863a76e547ebd30b074390284e911f07188ae
Olumide Olaoye <91534119+olsson990@users.noreply.github.com>
2026-02-04 22:51:07 -0600

    added not completed other project text

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! 🥰❤️ ;))</div>
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! 🥰❤️ : p.s I did not forget about the other project it's just crazy long;))</div>
         </>
       ) : (
         <>
